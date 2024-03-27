badf17f7b6c27bdfa510b386b4cab1bf8e7cc4fb
montanahouston <76497798+montanahouston@users.noreply.github.com>
2024-03-27 00:58:17 -0700

    added axios

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#weather-app-city");
+    cityElement.innerHTML = response.data.city;
+    
+    temperatureElement.innerHTML = Math.round(temperature)
+    
+}
+
+function searchCity(city) {
+let apiKey = "b34d09956e750a0813ab300t7o728b4f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#weather-app-city");
-    cityElement.innerHTML = searchInput.value;
-
+    
+searchCity(searchInput.value)
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Seattle");
