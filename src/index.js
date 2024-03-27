430bfc994b668588a7a940d39216e7636d455ad7
montanahouston <76497798+montanahouston@users.noreply.github.com>
2024-03-27 01:29:33 -0700

    injected weather data

--- src/index.js
+++ src/index.js
@@ -2,15 +2,37 @@ function refreshWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#weather-app-city");
-    cityElement.innerHTML = response.data.city;
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let speedElement = document.querySelector("#speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
     
+    cityElement.innerHTML = response.data.city;
+    timeElement.innerHTML = formatDate(date);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    speedElement.innerHTML = `${response.data.wind.speed} mph`;
+
     temperatureElement.innerHTML = Math.round(temperature)
     
 }
 
+function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thusday", "Friday", "Saturday", "Sunday"];
+    let day = days[date.getDay()];
+if (minbutes < 10) {
+    minutes = `0${minutes}`;
+}
+
+    return `${day} ${hours}:${minutes}`;
+}
+
 function searchCity(city) {
 let apiKey = "b34d09956e750a0813ab300t7o728b4f";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(refreshWeather);
 }
 
